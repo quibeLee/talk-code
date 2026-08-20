6c89fb1d5e19205d1940afc0a0171033e4e6830a
heng <lihengyx@outlook.com>
2026-08-21 01:07:30 +0800

    feat：后端项目 健康检查类修改

--- src/main/java/com/heng/hengaicode/controller/HealthController.java
+++ src/main/java/com/heng/hengaicode/controller/HealthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/health")
-public class HealthChekController {
+public class HealthController {
 
     @RequestMapping("/")
     public BaseResponse<String> check() {
